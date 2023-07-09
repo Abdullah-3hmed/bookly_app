137ccc99beadf48f09b71616afd9692bccf2884a
Abdullah-3hmed <abdullhahmed107@gmail.com>
2023-07-09 17:21:50 +0300

    modify book model

--- lib/features/home/data/models/BookModel.dart
+++ lib/features/home/data/models/BookModel.dart
@@ -10,16 +10,37 @@ class BookModel {
   late final VolumeInfo volumeInfo;
 }
 
+class SearchInfo {
+  SearchInfo({
+    this.textSnippet,
+  });
+
+  SearchInfo.fromJson(dynamic json) {
+    textSnippet = json['textSnippet'];
+  }
+
+  String? textSnippet;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['textSnippet'] = textSnippet;
+    return map;
+  }
+}
+
 class VolumeInfo {
   VolumeInfo({
     this.title,
     this.authors,
     this.imageLinks,
+    this.categories,
   });
 
   VolumeInfo.fromJson(dynamic json) {
     title = json['title'];
     authors = json['authors'] != null ? (json['authors'] as List<dynamic>).cast<String>() : [];
+    publisher = json['publisher'];
+    categories = json['categories'] != null ? (json['categories'] as List<dynamic>).cast<String>() : [];
 
     imageLinks = ImageLinks.fromJson(json['imageLinks']);
 
@@ -28,6 +49,9 @@ class VolumeInfo {
 
   String? title;
   List<String>? authors;
+  String? publisher;
+
+  List<String>? categories;
 
   ImageLinks? imageLinks;
 
